1cd51e72a2deb5cab2ea4a4e91de1e9b33d5aedd
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-04-09 11:01:58 +0300

    Lisätty sovellukseen entity-malli käyttäen Scaffold-DbContext-nimistä komentoa.

---
+++ AspNetWebDemo/AspNetWebDemo/Tietokanta/CategorySalesFor1997.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AspNetWebDemo.Tietokanta
+{
+    public partial class CategorySalesFor1997
+    {
+        public string CategoryName { get; set; }
+        public decimal? CategorySales { get; set; }
+    }
+}
---
+++ AspNetWebDemo/AspNetWebDemo/Tietokanta/ProductsAboveAveragePrice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AspNetWebDemo.Tietokanta
+{
+    public partial class ProductsAboveAveragePrice
+    {
+        public string ProductName { get; set; }
+        public decimal? UnitPrice { get; set; }
+    }
+}
