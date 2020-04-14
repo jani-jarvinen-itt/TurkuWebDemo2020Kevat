4c2c08fe3d45b295aede81c4c23b062ea7ad12cf
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-04-14 13:41:37 +0300

    Lisätty kolme uutta testi-metodia API-kontrolleriin.

--- AspNetWebDemo/AspNetWebDemo/Controllers/TestiController.cs
+++ AspNetWebDemo/AspNetWebDemo/Controllers/TestiController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetWebDemo.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,6 +22,47 @@ namespace AspNetWebDemo.Controllers
         public string Toka()
         {
             return "Tämä on toinen metodi.";
+        }
+
+        [Route("pvm")]
+        public DateTime Päivämäärä()
+        {
+            return DateTime.Now;
+        }
+
+        [Route("numerot")]
+        public List<int> PalautaNumeroLista()
+        {
+            List<int> numerot = new List<int>() { 1, 2, 3, 4, 5 };
+            return numerot;
+        }
+
+        [Route("asiakkaat")]
+        public List<Asiakas> HaeAsiakasLista()
+        {
+            List<Asiakas> asiakkaat = new List<Asiakas>()
+            {
+                new Asiakas()
+                {
+                    AsiakasId = 100,
+                    AsiakkaanNimi = "Yritys Oy",
+                    Sähköpostiosoite = "info@yritys.fi"
+                },
+                new Asiakas()
+                {
+                    AsiakasId = 101,
+                    AsiakkaanNimi = "Asiakas Oy",
+                    Sähköpostiosoite = "info@asiakas.fi"
+                },
+                new Asiakas()
+                {
+                    AsiakasId = 102,
+                    AsiakkaanNimi = "Testaus Oy",
+                    Sähköpostiosoite = "info@testaus.com"
+                }
+            };
+
+            return asiakkaat;
         }
     }
 }
