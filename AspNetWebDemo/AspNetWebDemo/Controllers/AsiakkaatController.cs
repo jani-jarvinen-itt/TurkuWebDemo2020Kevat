c12eb510dd3d1d5f25a0cbc11e33fd2b62d1b215
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-04-14 14:41:37 +0300

    Lisätty asiakas-kontrolleriin toiminnot asiakkaiden listaamiseen sekä uuden asiakkaan luontiin.

--- AspNetWebDemo/AspNetWebDemo/Controllers/AsiakkaatController.cs
+++ AspNetWebDemo/AspNetWebDemo/Controllers/AsiakkaatController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetWebDemo.Tietokanta;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,24 @@ namespace AspNetWebDemo.Controllers
     [ApiController]
     public class AsiakkaatController : ControllerBase
     {
+        [HttpPost]
+        [Route("")]
+        public bool LisääAsiakas(Customers asiakas)
+        {
+            NorthwindContext konteksti = new NorthwindContext();
+            konteksti.Customers.Add(asiakas);
+            konteksti.SaveChanges();
+
+            return true;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public List<Customers> HaeKaikki()
+        {
+            NorthwindContext konteksti = new NorthwindContext();
+            List<Customers> asiakkaat = konteksti.Customers.ToList();
+            return asiakkaat;
+        }
     }
 }
